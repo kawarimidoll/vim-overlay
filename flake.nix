f6a83f8b9b9d3ba0aee657d31a550fe93fcb6fc4
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-06 13:31:37 +1000

    style: use nixfmt-rfc-style instead of alejandra

--- flake.nix
+++ flake.nix
@@ -10,100 +10,108 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    vim-src,
-    pre-commit-hooks,
-  }: let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      vim-src,
+      pre-commit-hooks,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-    make-overlay = {
-      compiledby ? "vim-overlay",
-      cscope ? false,
-      lua ? false,
-      # perl ? false, # fail to build...
-      python3 ? false,
-      ruby ? false,
-      sodium ? false,
-    }: final: prev: {
-      vim = prev.vim.overrideAttrs (oldAttrs: {
-        version = "latest";
-        src = vim-src;
-        configureFlags =
-          (oldAttrs.configureFlags or [])
-          ++ [
-            "--with-compiledby=${compiledby}"
-            "--enable-fail-if-missing"
-          ]
-          ++ prev.lib.optionals lua [
-            "--with-lua-prefix=${prev.lua}"
-            "--enable-luainterp"
-          ]
-          ++ prev.lib.optionals python3 [
-            "--enable-python3interp=yes"
-            "--with-python3-command=${prev.python3}/bin/python3"
-            "--with-python3-config-dir=${prev.python3}/lib"
-            # Disable python2
-            "--disable-pythoninterp"
-          ]
-          # ++ prev.lib.optional perl "--enable-perlinterp"
-          ++ prev.lib.optionals ruby [
-            "--with-ruby-command=${prev.ruby}/bin/ruby"
-            "--enable-rubyinterp"
-          ]
-          ++ prev.lib.optional cscope "--enable-cscope";
+      make-overlay =
+        {
+          compiledby ? "vim-overlay",
+          cscope ? false,
+          lua ? false,
+          # perl ? false, # fail to build...
+          python3 ? false,
+          ruby ? false,
+          sodium ? false,
+        }:
+        final: prev: {
+          vim = prev.vim.overrideAttrs (oldAttrs: {
+            version = "latest";
+            src = vim-src;
+            configureFlags =
+              (oldAttrs.configureFlags or [ ])
+              ++ [
+                "--with-compiledby=${compiledby}"
+                "--enable-fail-if-missing"
+              ]
+              ++ prev.lib.optionals lua [
+                "--with-lua-prefix=${prev.lua}"
+                "--enable-luainterp"
+              ]
+              ++ prev.lib.optionals python3 [
+                "--enable-python3interp=yes"
+                "--with-python3-command=${prev.python3}/bin/python3"
+                "--with-python3-config-dir=${prev.python3}/lib"
+                # Disable python2
+                "--disable-pythoninterp"
+              ]
+              # ++ prev.lib.optional perl "--enable-perlinterp"
+              ++ prev.lib.optionals ruby [
+                "--with-ruby-command=${prev.ruby}/bin/ruby"
+                "--enable-rubyinterp"
+              ]
+              ++ prev.lib.optional cscope "--enable-cscope";
 
-        nativeBuildInputs =
-          (oldAttrs.nativeBuildInputs or [])
-          ++ [prev.pkg-config];
+            nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ prev.pkg-config ];
 
-        buildInputs =
-          (oldAttrs.buildInputs or [])
-          ++ [prev.ncurses prev.glib]
-          ++ prev.lib.optional lua prev.lua
-          ++ prev.lib.optional python3 prev.python3
-          ++ prev.lib.optional ruby prev.ruby
-          # ++ prev.lib.optional perl prev.perl
-          ++ prev.lib.optional sodium prev.libsodium;
-      });
-    };
-
-    vim-overlays = {
-      default = make-overlay {};
-      features = make-overlay;
-    };
-  in {
-    overlays = vim-overlays;
-
-    checks = forAllSystems (system: {
-      pre-commit-check = pre-commit-hooks.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          # format nix
-          alejandra.enable = true;
-          # format markdown
-          denofmt.enable = true;
+            buildInputs =
+              (oldAttrs.buildInputs or [ ])
+              ++ [
+                prev.ncurses
+                prev.glib
+              ]
+              ++ prev.lib.optional lua prev.lua
+              ++ prev.lib.optional python3 prev.python3
+              ++ prev.lib.optional ruby prev.ruby
+              # ++ prev.lib.optional perl prev.perl
+              ++ prev.lib.optional sodium prev.libsodium;
+          });
         };
-      };
-    });
 
-    devShells = forAllSystems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
+      vim-overlays = {
+        default = make-overlay { };
+        features = make-overlay;
       };
-      pre-commit-check = self.checks.${system}.pre-commit-check;
-    in {
-      default = pkgs.mkShell {
-        inherit (pre-commit-check) shellHook;
-        buildInputs = pre-commit-check.enabledPackages;
-      };
-    });
-  };
+    in
+    {
+      overlays = vim-overlays;
+
+      checks = forAllSystems (system: {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            # format nix
+            nixfmt-rfc-style.enable = true;
+            # format markdown
+            denofmt.enable = true;
+          };
+        };
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          pre-commit-check = self.checks.${system}.pre-commit-check;
+        in
+        {
+          default = pkgs.mkShell {
+            inherit (pre-commit-check) shellHook;
+            buildInputs = pre-commit-check.enabledPackages;
+          };
+        }
+      );
+    };
 }
